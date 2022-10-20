25d3a324e859ea445db472095b9b2996a37d5829
Wendy Wilgus <wilguswendy@gmail.com>
2022-10-20 18:19:51 -0400

    working on server html connection

--- Develop/server.js
+++ Develop/server.js
@@ -1,34 +1,62 @@
 
 const express = require('express');
-const app = express();
 const fs = require('fs');
 const path = require('path');
-const PORT = process.env.PORT || 3001;
+
+const app = express();
+const PORT = 3001;
 
 app.use(express.static('public'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-const savedNotes = require('./db/db.json';)
+const savedNotes = require('./db/db.json');
 
 
 
 app.get('/api/notes', (req, res) => {
     res.json(savedNotes.slice(1));
-})
+});
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'Develop/public/index.html'));
 });
 
-app.get('/', (req, res) =>  {
+app.get('/notes', (req, res) =>  {
     res.sendFile(path.join(__dirname, 'Develop/public/notes.html'));
 });
 
  //connects to index.html for all other routes
  app.get('/*', (req, res) => {
     res.sendFile(path.join(__dirname + 'Develop/public/index.html'));
+});
+
+//function to save user notes to json file
+function saveToDatabase(body, notesArray)   {
+    const newNote = body;
+
+    if (!Array.isArray(notesArray))
+        notesArray = [];
+    
+    if (notesArray.length === 0)
+        notesArray.push(0);
+
+    body.id = notesArray[0];
+    notesArray[0]++;
+    notesArray.push(newNote);
+    fs.writeFileSync(path.join(__dirname, './db/db.json', {encoding:'utf8', flag:'r'}),
+    JSON.stringify(notesArray, null, 2)
+    );
+
+    return newNote;
+}
+
+
+    fs.writeFile('Develop/db/db.json', JSON.stringify(notes, '\t'), err => {
+        if (err) throw err;
+        return true;
     });
+
 
 
 // Posts new notes to database
@@ -46,39 +74,33 @@ app.post('/api/notes', (req, res)  =>  {
 //join notes.html to api 
 app.get('/api/notes', (req, res) => {
     res.json(notes); //read the json file and return all notes 
-    });
+});
 
 
 
 
 
-//function to save user notes to json file
-function saveToDatabase(body, notesArray)   {
-    const newNote = body;
 
-    fs.writeFile('Develop/db/db.json', JSON.stringify(notes, '\t'), err => {
-        if (err) throw err;
-        return true;
-    });
 
     
 
-//gets notes information based on specific id
-app.get('/api/notes/:id', (req, res)  =>  {
-    res.json(notes[req.params.id]);
-});
+    //gets notes information based on specific id
+    app.get('/api/notes/:id', (req, res)  =>  {
+        res.json(notes[req.params.id]);
+    });
 
-//deletes notes based on specific id
-app.delete('./api/notes/:id', (req, res)    =>  {
-    notes.splic(req.params.id, 1);
-    saveToDatabase();
-});
+    //deletes notes based on specific id
+    app.delete('./api/notes/:id', (req, res)    =>  {
+        notes.splic(req.params.id, 1);
+        saveToDatabase();
+    });
 
-//show notes on notes.html
-app.get('/notes', (req, res)    =>  {
-    res.sendFile(path.join(__dirname + 'Develop/public/notes.html'));
-});
+    //show notes on notes.html
+    app.get('/notes', (req, res)    =>  {
+        res.sendFile(path.join(__dirname + 'Develop/public/notes.html'));
+    });
+}
 
-app.listen(3001, () => {
-console.log("express server listening on port 3001");
+app.listen(PORT, () => {
+console.log("express server listening on port ${PORT} 🚀");
 });
