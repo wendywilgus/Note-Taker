21bc54ef772f99981edbe3c8899dc83299e7b98b
Wendy Wilgus <wilguswendy@gmail.com>
2022-10-20 21:46:13 -0400

    trying to connect server

--- Develop/server.js
+++ Develop/server.js
@@ -10,97 +10,74 @@ app.use(express.static('public'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-const savedNotes = require('./db/db.json');
-
-
-
-app.get('/api/notes', (req, res) => {
-    res.json(savedNotes.slice(1));
-});
-
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'Develop/public/index.html'));
 });
 
 app.get('/notes', (req, res) =>  {
-    res.sendFile(path.join(__dirname, 'Develop/public/notes.html'));
+    res.sendFile(path.join(__dirname, '/Users/wendywilgus/git/wendywilgus/Note Taker/Develop/public/notes.html'));
 });
 
- //connects to index.html for all other routes
- app.get('/*', (req, res) => {
-    res.sendFile(path.join(__dirname + 'Develop/public/index.html'));
+
+app.get('/api/notes', (req, res) => {
+    fs.readFile('/Users/wendywilgus/git/wendywilgus/Note Taker/Develop/db/db.json', 'utf8', (err, data) =>  {
+        const noteText = JSON.parse(data);
+        res.json(noteText);
 });
-
-//function to save user notes to json file
-function saveToDatabase(body, notesArray)   {
-    const newNote = body;
-
-    if (!Array.isArray(notesArray))
-        notesArray = [];
-    
-    if (notesArray.length === 0)
-        notesArray.push(0);
-
-    body.id = notesArray[0];
-    notesArray[0]++;
-    notesArray.push(newNote);
-    fs.writeFileSync(path.join(__dirname, './db/db.json', {encoding:'utf8', flag:'r'}),
-    JSON.stringify(notesArray, null, 2)
-    );
-
-    return newNote;
-}
-
-
-    fs.writeFile('Develop/db/db.json', JSON.stringify(notes, '\t'), err => {
-        if (err) throw err;
-        return true;
-    });
-
-
-
+ 
 // Posts new notes to database
 app.post('/api/notes', (req, res)  =>  {
-    fs.readFileSync('./db/db.json', {encoding:'utf8', flag:'r'});
-    let newNote = req.body;
-    notes.push( {
-        title: req.body.title,
-        text:  req.body.text});
-    saveToDatabase();
-    return newNote;
-    
+    const {title, text} = req.body;
+
+    if (title && text)  {
+        const newNote = {
+            title,
+            text,
+            id,
+        };
+
+        fs.readFile('/Users/wendywilgus/git/wendywilgus/Note Taker/Develop/db/db.json', 'utf8', (err, data) =>  {
+            const noteText = JSON.parse(data);
+
+            noteText.push(newNote);
+
+            fs.writeFile('/Users/wendywilgus/git/wendywilgus/Note Taker/Develop/db/db.json', JSON.stringify(noteText, null, 4), (err));
+        });
+    }
+
+    const response = {
+        status: 'sucess',
+        body: newNote,
+        };
 });
 
-//join notes.html to api 
-app.get('/api/notes', (req, res) => {
-    res.json(notes); //read the json file and return all notes 
+
+
+ //gets note by id
+app.get('/api/notes/:id', (req, res) => {
+    fs.readFile('/Users/wendywilgus/git/wendywilgus/Note Taker/Develop/db/db.json', 'utf8', (err, data) => {
+        const noteText = JSON.parse(data);
+        res.json(noteText[req.param.id]);
+    }
+
+)
 });
 
-
-
-
-
-
-
-    
-
-    //gets notes information based on specific id
-    app.get('/api/notes/:id', (req, res)  =>  {
-        res.json(notes[req.params.id]);
-    });
-
-    //deletes notes based on specific id
-    app.delete('./api/notes/:id', (req, res)    =>  {
-        notes.splic(req.params.id, 1);
-        saveToDatabase();
-    });
-
-    //show notes on notes.html
-    app.get('/notes', (req, res)    =>  {
-        res.sendFile(path.join(__dirname + 'Develop/public/notes.html'));
-    });
-}
-
-app.listen(PORT, () => {
-console.log("express server listening on port ${PORT} 🚀");
-});
+//deletes notes based on specific id
+app.delete("/api/notes/:id", function(req, res) {
+    fs.readFile('./db/db.json', 'utf8', (err, data) => {
+      const noteText = JSON.parse(data);
+      noteText.splice(req.params.id, 1); 
+      fs.writeFile(`./db/db.json`, JSON.stringify(noteText, null, 4), (err) => err
+                ? console.error(err)
+                : console.log(
+                    `Deleted note.`)
+              );
+      });
+      res.redirect('back'); 
+    }
+    )
+  
+  app.listen(PORT, () =>
+    console.log(`express server listening on port ${PORT} 🚀`)
+)
