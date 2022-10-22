7a6bc7b8035753bff5436cbb12fb4731c02e232d
Wendy Wilgus <wilguswendy@gmail.com>
2022-10-22 11:15:58 -0400

    changed Port

--- Develop/server.js
+++ Develop/server.js
@@ -4,7 +4,7 @@ const fs = require('fs');
 const path = require('path');
 
 const app = express();
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 
 app.use(express.static('public'));
 app.use(express.json());
